25353e73ad5c9db9f955568e209e5c844a503cd6
luiq54 <dev.darshanphaldesai@gmail.com>
2022-11-07 13:09:42 +0530

    Fix cmake for linux

--- cmake/wxWidgets.cmake
+++ cmake/wxWidgets.cmake
@@ -2,7 +2,7 @@ include(FetchContent)
 
 FetchContent_Declare(
 	wxWidgets
-	URL https://github.com/wxWidgets/wxWidgets/releases/download/v3.1.4/wxWidgets-3.1.4.zip
+	URL https://github.com/wxWidgets/wxWidgets/releases/download/v3.1.4/wxWidgets-3.1.4.tar.bz2
 )
 FetchContent_GetProperties(wxWidgets)
 if(NOT wxwidgets_POPULATED)
@@ -10,7 +10,7 @@ if(NOT wxwidgets_POPULATED)
 endif()
 
 set(CMAKE_MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>")
-set(wxBUILD_MONOLITHIC ON CACHE INTERNAL "")
-set(wxBUILD_SHARED OFF CACHE INTERNAL "")
+set(wxBUILD_MONOLITHIC OFF CACHE INTERNAL "")
+set(wxBUILD_SHARED ON CACHE INTERNAL "")
 set(wxBUILD_USE_STATIC_RUNTIME ON CACHE INTERNAL "")
 add_subdirectory("${wxwidgets_SOURCE_DIR}")
